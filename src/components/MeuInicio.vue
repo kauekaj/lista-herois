7654f1e6d3448de4caa88659cebaa040e8128109
Kauê Jacyntho <kauekaj@gmail.com>
2022-03-18 11:29:23 -0300

    create components MeuInico && AppTopBar

---
+++ src/components/MeuInicio.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-container>
+         <h2 class="text-h5 text-center mb-3 mt-3">Início</h2>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'MeuInicio'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/shared/AppTopBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="app-top-bar">
+    <v-app-bar app color="red darken-1" dense dark>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+      <v-toolbar-title>Lista de Heróis</v-toolbar-title>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav dense>
+        <v-list-item-group v-model="group" active-class="red lighten-1">
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Início</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Heróis</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+    
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+}
+</script>

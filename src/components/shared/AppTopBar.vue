5774f8885a34c8ace3b8b0b403ed0d3c116c2c17
Kauê Jacyntho <kauekaj@gmail.com>
2022-03-18 12:03:45 -0300

    add page navegation functionality

--- src/components/shared/AppTopBar.vue
+++ src/components/shared/AppTopBar.vue
@@ -7,19 +7,23 @@
     <v-navigation-drawer v-model="drawer" absolute temporary>
       <v-list nav dense>
         <v-list-item-group v-model="group" active-class="red lighten-1">
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Início</v-list-item-title>
-          </v-list-item>
+          <router-link to="/">
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-home</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Início</v-list-item-title>
+            </v-list-item>
+          </router-link>
 
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Heróis</v-list-item-title>
-          </v-list-item>
+          <router-link to="/listadeherois">
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-account</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Heróis</v-list-item-title>
+            </v-list-item>
+          </router-link>
         </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
@@ -28,10 +32,15 @@
 
 <script>
 export default {
-    
   data: () => ({
     drawer: false,
     group: null,
   }),
-}
+};
 </script>
+
+<style scoped>
+  .app-top-bar a {
+    text-decoration: none;
+  } 
+</style>
